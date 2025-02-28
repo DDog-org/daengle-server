ca8b781df39b78ad53353c3850b4a7d2960b0538
hyoseok <alexization@gmail.com>
2025-02-28 18:09:47 +0900

    [refactoring/daengle-131] Groomer 모듈 @AuthPayload 어노테이션 적용

--- daengle-groomer-api/src/main/java/ddog/groomer/presentation/estimate/EstimateController.java
+++ daengle-groomer-api/src/main/java/ddog/groomer/presentation/estimate/EstimateController.java
@@ -1,5 +1,6 @@
 package ddog.groomer.presentation.estimate;
 
+import ddog.auth.annotation.AuthPayload;
 import ddog.auth.dto.PayloadDto;
 import ddog.auth.exception.common.CommonResponseEntity;
 import ddog.groomer.application.EstimateService;
@@ -24,7 +25,7 @@ public class EstimateController {
     /* (신규) 일반 견적서들 리스트 조회 */
     @GetMapping("/general/list")
     public CommonResponseEntity<EstimateInfo.General> findGeneralEstimates(
-            PayloadDto payloadDto,
+            @AuthPayload PayloadDto payloadDto,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "20") int size
     ) {
@@ -34,7 +35,7 @@ public class EstimateController {
     /* (신규) 지정 견적서들 리스트 조회 */
     @GetMapping("/designation/list")
     public CommonResponseEntity<EstimateInfo.Designation> findDesignationEstimates(
-            PayloadDto payloadDto,
+            @AuthPayload PayloadDto payloadDto,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "20") int size
     ) {
@@ -49,7 +50,8 @@ public class EstimateController {
 
     /* 미용사 -> 사용자 (대기) 미용 견적서 작성 */
     @PostMapping
-    public CommonResponseEntity<EstimateResp> createEstimate(@RequestBody CreatePendingEstimateReq request, PayloadDto payloadDto) {
+    public CommonResponseEntity<EstimateResp> createEstimate(@RequestBody CreatePendingEstimateReq request,
+                                                             @AuthPayload PayloadDto payloadDto) {
         EstimateInfo.EstimateUserInfo savedEstimate = estimateService.findByUserInfoByEstimateId(request.getId());
         return success(estimateService.createPendingEstimate(request, payloadDto.getAccountId()));
     }
